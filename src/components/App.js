4c21f31c3f451746ab2d509ba99a1ffea81211a5
Marikita1007 <ishinomaki1007@gmail.com>
2024-05-31 22:37:16 +0200

    #React-88 : Finished course : Moving Components Into Separate Files

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Form from "./Form";
+import Logo from "./Logo";
+import PackingList from "./PackingList";
+import Status from "./Status";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 3, packed: false },
+];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all itema ?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onDeleteAllItems={handleClearList}
+      />
+      <Status items={items} />
+    </div>
+  );
+}
---
+++ src/components/Status.js
@@ -0,0 +1,22 @@
+function Status({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      {percentage === 100
+        ? "You got everything ! Ready to go ✈️"
+        : `💼 You have ${numItems} item on you list, and you already packed ${numPacked} (${percentage}%) `}
+    </footer>
+  );
+}
+
+export default Status;
